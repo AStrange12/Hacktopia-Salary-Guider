d5741a6432f497f5bef170085088840171a83a4b
JAUHAR EAMAM <jauhareamam389@gmail.com>
2025-12-21 16:02:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4686982573-9f543",
+  "appId": "1:1076401669474:web:4508b5c181e3dff16cd83b",
+  "apiKey": "AIzaSyA-toy6WOjQgkb4O4DEz-f4IOQdWUibVvg",
+  "authDomain": "studio-4686982573-9f543.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1076401669474"
+};
