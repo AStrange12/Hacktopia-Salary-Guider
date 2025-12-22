29ee0844fa80d71cf7d15561ab65ca191d21af3e
JAUHAR EAMAM <jauhareamam389@gmail.com>
2025-12-22 14:23:23 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/ai/flows/index.ts
+++
@@ -1,5 +0,0 @@
-'use server';
-
-export { analyzeSpendingBehavior } from './analyze-spending-behavior';
-export { suggestPersonalizedAdvice } from './suggest-personalized-advice';
-export { summarizeMonthlySpending } from './summarize-monthly-spending';
